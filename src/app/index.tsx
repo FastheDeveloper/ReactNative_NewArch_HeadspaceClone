6ae8d1b9ef7154be8912e192166cac961ae0a4ab
Farouq Seriki <farouq@basaltx.io>
2024-11-07 15:54:42 +0100

    homescreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,15 @@
-import { Text } from "react-native";
+import { FlatList } from "react-native";
+import { meditations } from "../data";
+import MeditationListItem from "../components/MeditationListItem";
 
-export default function HomeScreen(){
-    return (
-        <Text className="text-3xl">Hello</Text>
-    )
+export default function HomeScreen() {
+  return (
+    <FlatList
+      data={meditations}
+      renderItem={({ item }) => <MeditationListItem meditation={item} />}
+      contentContainerClassName="gap-5 p-3"
+      className="bg-white"
+    //   contentContainerStyle={{gap:20}}
+    />
+  );
 }
